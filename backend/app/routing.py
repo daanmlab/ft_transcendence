aec8fc62e84fdee0286054fbe3b8ef7c60c11e3b
daanmlab <daanmlab@gmail.com>
2024-08-26 17:53:17 +0100

    chore: Update PongConsumer to handle game creation and joining

--- backend/app/routing.py
+++ backend/app/routing.py
@@ -1,12 +1,10 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import re_path
+from django.urls import re_path, path
 from app import pong
 
 # URLs that handle the WebSocket connection are placed here.
 websocket_urlpatterns=[
-    re_path(
-        "ws/yea", pong.PongConsumer.as_asgi()
-    ),
+    path("ws/<str:game>/", pong.PongConsumer.as_asgi()),
+    re_path("ws/", pong.PongConsumer.as_asgi()),
 ]
 
